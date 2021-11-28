baadb90bf41d2970f8b29305a47312f683061e54
Veniamin <venyasss@yandex.ru>
2021-11-28 20:33:55 +0300

    refactor: add alt attribute for media
    according to:
    1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="task__delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt='remove';
     deleteButtonImg.className='task__delete-image';
     deleteButton.appendChild(deleteButtonImg);
 
